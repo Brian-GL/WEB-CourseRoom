fec13ba3816ea783987a64efae0cd4bbca6bdd7b
joe-br <bugarin.1999@hotmail.com>
2022-12-29 22:05:22 -0600

    Agregados endpoints en Avisos, Catalogos y Chats.

--- app/Http/Controllers/CatalogosController.php
+++ app/Http/Controllers/CatalogosController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AvisosController extends Controller
+class CatalogosController extends Controller
 {
     #region Views
 
@@ -12,13 +12,12 @@ class AvisosController extends Controller
 
     #region AJAX
 
-    public function aviso_actualizar(Request $request)
+    public function catalogocursoestatus_obtener(Request $request)
     {
         try {
 
             $validator = Validator::make($request->all(), [
-                'IdAviso' => ['required'],
-                'IdUsuario' => ['required']
+                
             ]);
 
             if ($validator->fails()) {
@@ -27,70 +26,14 @@ class AvisosController extends Controller
 
                 $url = env('COURSEROOM_API');
 
-                $idAviso = $request->input('IdAviso');
-                $idUsuario = $request->input('IdUsuario');
+                $idEstatus = $request->input('IdEstatus');
 
                 if($url != ''){
 
                     $response = Http::withHeaders([
                         'Authorization' => env('COURSEROOM_API_KEY'),
-                    ])->put($url.'/api/avisos/actualizar', [
-                        'IdAviso' => $idAviso,
-                        'IdUsuario' => $idUsuario
-                    ]);
-
-                    if ($response->ok()){
-
-                        $result = json_decode($response->body());
-
-                        return response()->json(['code' => 200 , 'data' => $result], 200);
-
-                    } else{
-                        return response()->json(['code' => 500 , 'data' => $response->body()], 200);
-                    }
-
-                } else{
-                    return response()->json(['code' => 404 , 'data' => 'Empty url'], 200);
-                }
-            }
-
-        } catch (\Throwable $th) {
-            return response()->json(['code' => 500 , 'data' => $th->getMessage()], 200);
-        }
-
-    }
-
-    public function aviso_registrar(Request $request)
-    {
-        try {
-
-            $validator = Validator::make($request->all(), [
-                'IdUsuario' => ['required'],
-                'Aviso' => ['required'],
-                'Descripcion' => ['required'],
-                'IdTipoAviso' => ['required']
-            ]);
-
-            if ($validator->fails()) {
-                return response()->json(['code' => 404 , 'data' => $validator->errors()->first()], 200);
-            } else {
-
-                $url = env('COURSEROOM_API');
-
-                $idUsuario = $request->input('IdUsuario');
-                $aviso = $request->input('Aviso');
-                $descripcion = $request->input('Descripcion');
-                $idTipoAviso = $request->input('IdTipoAviso');
-
-                if($url != ''){
-
-                    $response = Http::withHeaders([
-                        'Authorization' => env('COURSEROOM_API_KEY'),
-                    ])->post($url.'/api/avisos/registrar', [
-                        'IdUsuario' => $idUsuario,
-                        'Aviso' => $aviso,
-                        'Descripcion' => $descripcion,
-                        'IdTipoAviso' => $idTipoAviso
+                    ])->post($url.'/api/catalogos/cursoestatus', [
+                        'IdEstatus' => $idEstatus
                     ]);
 
                     if ($response->ok()){
@@ -113,13 +56,12 @@ class AvisosController extends Controller
         }
     }
 
-    public function aviso_remover(Request $request)
+    public function catalogoestados_obtener(Request $request)
     {
         try {
 
             $validator = Validator::make($request->all(), [
-                'IdUsuario' => ['required'],
-                'IdAviso' => ['required']
+                
             ]);
 
             if ($validator->fails()) {
@@ -128,61 +70,14 @@ class AvisosController extends Controller
 
                 $url = env('COURSEROOM_API');
 
-                $idUsuario = $request->input('IdUsuario');
-                $idAviso = $request->input('IdAviso');
+                $idEstado = $request->input('IdEstado');
 
                 if($url != ''){
 
                     $response = Http::withHeaders([
                         'Authorization' => env('COURSEROOM_API_KEY'),
-                    ])->delete($url.'/api/avisos/remover', [
-                        'IdUsuario' => $idUsuario,
-                        'IdAviso' => $idAviso
-                    ]);
-
-                    if ($response->ok()){
-
-                        $result = json_decode($response->body());
-
-                        return response()->json(['code' => 200 , 'data' => $result], 200);
-
-                    } else{
-                        return response()->json(['code' => 500 , 'data' => $response->body()], 200);
-                    }
-
-                } else{
-                    return response()->json(['code' => 404 , 'data' => 'Empty url'], 200);
-                }
-            }
-
-        } catch (\Throwable $th) {
-            return response()->json(['code' => 500 , 'data' => $th->getMessage()], 200);
-        }
-
-    }
-
-    public function avisodetalle_obtener(Request $request)
-    {
-        try {
-
-            $validator = Validator::make($request->all(), [
-                'IdAviso' => ['required']
-            ]);
-
-            if ($validator->fails()) {
-                return response()->json(['code' => 404 , 'data' => $validator->errors()->first()], 200);
-            } else {
-
-                $url = env('COURSEROOM_API');
-
-                $idAviso = $request->input('IdAviso');
-
-                if($url != ''){
-
-                    $response = Http::withHeaders([
-                        'Authorization' => env('COURSEROOM_API_KEY'),
-                    ])->post($url.'/api/avisos/detalle', [
-                        'IdAviso' => $idAviso
+                    ])->post($url.'/api/catalogos/estados', [
+                        'IdEstado' => $idEstado
                     ]);
 
                     if ($response->ok()){
@@ -205,15 +100,12 @@ class AvisosController extends Controller
         }
     }
 
-    public function avisoplagioprofesor_registrar(Request $request)
+    public function catalogotareapendienteestatus_obtener(Request $request)
     {
         try {
 
             $validator = Validator::make($request->all(), [
-                'IdProfesor' => ['required'],
-                'IdUsuario' => ['required'],
-                'IdTarea' => ['required'],
-                'NombreArchivo' => ['required']
+                
             ]);
 
             if ($validator->fails()) {
@@ -222,20 +114,14 @@ class AvisosController extends Controller
 
                 $url = env('COURSEROOM_API');
 
-                $idProfesor = $request->input('IdProfesor');
-                $idUsuario = $request->input('IdUsuario');
-                $idTarea = $request->input('IdTarea');
-                $nombreArchivo = $request->input('NombreArchivo');
+                $idEstatusTareaPendiente = $request->input('IdEstatusTareaPendiente');
 
                 if($url != ''){
 
                     $response = Http::withHeaders([
                         'Authorization' => env('COURSEROOM_API_KEY'),
-                    ])->post($url.'/api/avisos/plagioprofesor', [
-                        'IdProfesor' => $idProfesor,
-                        'IdUsuario' => $idUsuario,
-                        'IdTarea' => $idTarea,
-                        'NombreArchivo' => $nombreArchivo
+                    ])->post($url.'/api/catalogos/estatustareapendiente', [
+                        'IdEstatusTareaPendiente' => $idEstatusTareaPendiente
                     ]);
 
                     if ($response->ok()){
@@ -258,12 +144,12 @@ class AvisosController extends Controller
         }
     }
 
-    public function avisos_obtener(Request $request)
+    public function catalogolocalidades_obtener(Request $request)
     {
         try {
 
             $validator = Validator::make($request->all(), [
-                'IdUsuario' => ['required']
+                
             ]);
 
             if ($validator->fails()) {
@@ -272,16 +158,16 @@ class AvisosController extends Controller
 
                 $url = env('COURSEROOM_API');
 
-                $idUsuario = $request->input('IdUsuario');
-                $leido = $request->input('Leido');
+                $idEstado = $request->input('IdEstado');
+                $idLocalidad = $request->input('IdLocalidad');
 
                 if($url != ''){
 
                     $response = Http::withHeaders([
                         'Authorization' => env('COURSEROOM_API_KEY'),
-                    ])->post($url.'/api/avisos/obtener', [
-                        'IdUsuario' => $idUsuario,
-                        'Leido' => $leido
+                    ])->post($url.'/api/catalogos/localidades', [
+                        'IdEstado' => $idEstado,
+                        'Idlocalidad' => $idLocalidad
                     ]);
 
                     if ($response->ok()){
@@ -304,12 +190,12 @@ class AvisosController extends Controller
         }
     }
 
-    public function avisos_validar(Request $request)
+    public function catalogopreguntarespuestaestatus_obtener(Request $request)
     {
         try {
 
             $validator = Validator::make($request->all(), [
-                'IdUsuario' => ['required']
+                
             ]);
 
             if ($validator->fails()) {
@@ -318,16 +204,146 @@ class AvisosController extends Controller
 
                 $url = env('COURSEROOM_API');
 
-                $idUsuario = $request->input('IdUsuario');
-                $fechaVisualizacion = $request->input('FechaVisualizacion');
+                $idEstatusPreguntaRespuesta = $request->input('IdEstatusPreguntaRespuesta');
 
                 if($url != ''){
 
                     $response = Http::withHeaders([
                         'Authorization' => env('COURSEROOM_API_KEY'),
-                    ])->post($url.'/api/avisos/validar', [
-                        'IdUsuario' => $idUsuario,
-                        'FechaVisualizacion' => $fechaVisualizacion
+                    ])->post($url.'/api/catalogos/preguntarespuesta', [
+                        'IdEstatusPreguntaRespuesta' => $idEstatusPreguntaRespuesta
+                    ]);
+
+                    if ($response->ok()){
+
+                        $result = json_decode($response->body());
+
+                        return response()->json(['code' => 200 , 'data' => $result], 200);
+
+                    } else{
+                        return response()->json(['code' => 500 , 'data' => $response->body()], 200);
+                    }
+
+                } else{
+                    return response()->json(['code' => 404 , 'data' => 'Empty url'], 200);
+                }
+            }
+
+        } catch (\Throwable $th) {
+            return response()->json(['code' => 500 , 'data' => $th->getMessage()], 200);
+        }
+    }
+
+    public function catalogopreguntascuestionario_obtener(Request $request)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['code' => 404 , 'data' => $validator->errors()->first()], 200);
+            } else {
+
+                $url = env('COURSEROOM_API');
+
+                $idCuestionario = $request->input('IdCuestionario');
+
+                if($url != ''){
+
+                    $response = Http::withHeaders([
+                        'Authorization' => env('COURSEROOM_API_KEY'),
+                    ])->post($url.'/api/catalogos/preguntascuestionario', [
+                        'IdCuestionario' => $idCuestionario
+                    ]);
+
+                    if ($response->ok()){
+
+                        $result = json_decode($response->body());
+
+                        return response()->json(['code' => 200 , 'data' => $result], 200);
+
+                    } else{
+                        return response()->json(['code' => 500 , 'data' => $response->body()], 200);
+                    }
+
+                } else{
+                    return response()->json(['code' => 404 , 'data' => 'Empty url'], 200);
+                }
+            }
+
+        } catch (\Throwable $th) {
+            return response()->json(['code' => 500 , 'data' => $th->getMessage()], 200);
+        }
+    }
+
+    public function catalogotematicas_obtener(Request $request)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['code' => 404 , 'data' => $validator->errors()->first()], 200);
+            } else {
+
+                $url = env('COURSEROOM_API');
+
+                $idTematica = $request->input('IdTematica');
+
+                if($url != ''){
+
+                    $response = Http::withHeaders([
+                        'Authorization' => env('COURSEROOM_API_KEY'),
+                    ])->post($url.'/api/catalogos/tematicas', [
+                        'IdTematica' => $idTematica
+                    ]);
+
+                    if ($response->ok()){
+
+                        $result = json_decode($response->body());
+
+                        return response()->json(['code' => 200 , 'data' => $result], 200);
+
+                    } else{
+                        return response()->json(['code' => 500 , 'data' => $response->body()], 200);
+                    }
+
+                } else{
+                    return response()->json(['code' => 404 , 'data' => 'Empty url'], 200);
+                }
+            }
+
+        } catch (\Throwable $th) {
+            return response()->json(['code' => 500 , 'data' => $th->getMessage()], 200);
+        }
+    }
+
+    public function catalogotiposusuario_obtener(Request $request)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['code' => 404 , 'data' => $validator->errors()->first()], 200);
+            } else {
+
+                $url = env('COURSEROOM_API');
+
+                $idTipoUsuario = $request->input('IdTipoUsuario');
+
+                if($url != ''){
+
+                    $response = Http::withHeaders([
+                        'Authorization' => env('COURSEROOM_API_KEY'),
+                    ])->post($url.'/api/catalogos/tiposusuario', [
+                        'IdTipoUsuario' => $idTipoUsuario
                     ]);
 
                     if ($response->ok()){
--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -27,16 +27,312 @@ class ChatsController extends Controller
 
                 $url = env('COURSEROOM_API');
 
-                $IdUsuarioEmisor = $request->input('IdUsuarioEmisor');
-                $IdUsuarioReceptor = $request->input('IdUsuarioReceptor');
+                $idUsuarioEmisor = $request->input('IdUsuarioEmisor');
+                $idUsuarioReceptor = $request->input('IdUsuarioReceptor');
 
                 if($url != ''){
 
                     $response = Http::withHeaders([
                         'Authorization' => env('COURSEROOM_API_KEY'),
                     ])->post($url.'/api/chats/registrar', [
-                        'idUsuarioEmisor' => $IdUsuarioEmisor,
-                        'idUsuarioReceptor' => $IdUsuarioReceptor
+                        'IdUsuarioEmisor' => $idUsuarioEmisor,
+                        'IdUsuarioReceptor' => $idUsuarioReceptor
+                    ]);
+
+                    if ($response->ok()){
+
+                        $result = json_decode($response->body());
+
+                        return response()->json(['code' => 200 , 'data' => $result], 200);
+
+                    } else{
+                        return response()->json(['code' => 500 , 'data' => $response->body()], 200);
+                    }
+
+                } else{
+                    return response()->json(['code' => 404 , 'data' => 'Empty url'], 200);
+                }
+            }
+
+        } catch (\Throwable $th) {
+            return response()->json(['code' => 500 , 'data' => $th->getMessage()], 200);
+        }
+
+    }
+
+    public function chat_remover(Request $request)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'IdChat' => ['required'],
+                'IdUsuario' => ['required']
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['code' => 404 , 'data' => $validator->errors()->first()], 200);
+            } else {
+
+                $url = env('COURSEROOM_API');
+
+                $idChat = $request->input('IdChat');
+                $idUsuario = $request->input('IdUsuario');
+
+                if($url != ''){
+
+                    $response = Http::withHeaders([
+                        'Authorization' => env('COURSEROOM_API_KEY'),
+                    ])->delete($url.'/api/chats/remover', [
+                        'IdChat' => $idChat,
+                        'IdUsuario' => $idUsuario
+                    ]);
+
+                    if ($response->ok()){
+
+                        $result = json_decode($response->body());
+
+                        return response()->json(['code' => 200 , 'data' => $result], 200);
+
+                    } else{
+                        return response()->json(['code' => 500 , 'data' => $response->body()], 200);
+                    }
+
+                } else{
+                    return response()->json(['code' => 404 , 'data' => 'Empty url'], 200);
+                }
+            }
+
+        } catch (\Throwable $th) {
+            return response()->json(['code' => 500 , 'data' => $th->getMessage()], 200);
+        }
+
+    }
+
+    public function chatmensaje_registrar(Request $request)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'IdChat' => ['required'],
+                'IdUsuarioEmisor' => ['required'],
+                'Mensaje' => ['required']
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['code' => 404 , 'data' => $validator->errors()->first()], 200);
+            } else {
+
+                $url = env('COURSEROOM_API');
+
+                $idChat = $request->input('IdChat');
+                $idUsuarioEmisor = $request->input('IdUsuarioEmisor');
+                $mensaje = $request->input('Mensaje');
+                $archivo = $request->input('Archivo');
+
+                if($url != ''){
+
+                    $response = Http::withHeaders([
+                        'Authorization' => env('COURSEROOM_API_KEY'),
+                    ])->post($url.'/api/chats/mensajeregistrar', [
+                        'IdChat' => $idChat,
+                        'IdUsuarioEmisor' => $idUsuarioEmisor,
+                        'Mensaje' => $mensaje,
+                        'Archivo' => $archivo
+                    ]);
+
+                    if ($response->ok()){
+
+                        $result = json_decode($response->body());
+
+                        return response()->json(['code' => 200 , 'data' => $result], 200);
+
+                    } else{
+                        return response()->json(['code' => 500 , 'data' => $response->body()], 200);
+                    }
+
+                } else{
+                    return response()->json(['code' => 404 , 'data' => 'Empty url'], 200);
+                }
+            }
+
+        } catch (\Throwable $th) {
+            return response()->json(['code' => 500 , 'data' => $th->getMessage()], 200);
+        }
+
+    }
+
+    public function chatmensaje_remover(Request $request)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'IdChat' => ['required'],
+                'IdUsuarioEmisor' => ['required'],
+                'IdMensaje' => ['required']
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['code' => 404 , 'data' => $validator->errors()->first()], 200);
+            } else {
+
+                $url = env('COURSEROOM_API');
+
+                $idChat = $request->input('IdChat');
+                $idUsuarioEmisor = $request->input('IdUsuarioEmisor');
+                $idMensaje = $request->input('IdMensaje');
+
+                if($url != ''){
+
+                    $response = Http::withHeaders([
+                        'Authorization' => env('COURSEROOM_API_KEY'),
+                    ])->delete($url.'/api/chats/mensajeremover', [
+                        'IdChat' => $idChat,
+                        'IdUsuarioEmisor' => $idUsuarioEmisor,
+                        'IdMensaje' => $idMensaje
+                    ]);
+
+                    if ($response->ok()){
+
+                        $result = json_decode($response->body());
+
+                        return response()->json(['code' => 200 , 'data' => $result], 200);
+
+                    } else{
+                        return response()->json(['code' => 500 , 'data' => $response->body()], 200);
+                    }
+
+                } else{
+                    return response()->json(['code' => 404 , 'data' => 'Empty url'], 200);
+                }
+            }
+
+        } catch (\Throwable $th) {
+            return response()->json(['code' => 500 , 'data' => $th->getMessage()], 200);
+        }
+
+    }
+
+    public function chatmensajes_obtener(Request $request)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'IdChat' => ['required'],
+                'Ultimo' => ['required']
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['code' => 404 , 'data' => $validator->errors()->first()], 200);
+            } else {
+
+                $url = env('COURSEROOM_API');
+
+                $idChat = $request->input('IdChat');
+                $ultimo = $request->input('Ultimo');
+
+                if($url != ''){
+
+                    $response = Http::withHeaders([
+                        'Authorization' => env('COURSEROOM_API_KEY'),
+                    ])->post($url.'/api/chats/mensajesobtener', [
+                        'IdChat' => $idChat,
+                        'Ultimo' => $ultimo
+                    ]);
+
+                    if ($response->ok()){
+
+                        $result = json_decode($response->body());
+
+                        return response()->json(['code' => 200 , 'data' => $result], 200);
+
+                    } else{
+                        return response()->json(['code' => 500 , 'data' => $response->body()], 200);
+                    }
+
+                } else{
+                    return response()->json(['code' => 404 , 'data' => 'Empty url'], 200);
+                }
+            }
+
+        } catch (\Throwable $th) {
+            return response()->json(['code' => 500 , 'data' => $th->getMessage()], 200);
+        }
+
+    }
+
+    public function chats_buscar(Request $request)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'IdUsuarioEmisor' => ['required']
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['code' => 404 , 'data' => $validator->errors()->first()], 200);
+            } else {
+
+                $url = env('COURSEROOM_API');
+
+                $idUsuarioEmisor = $request->input('IdUsuarioEmisor');
+                $nombre = $request->input('Nombre');
+                $paterno = $request->input('Paterno');
+                $materno = $request->input('Materno');
+
+                if($url != ''){
+
+                    $response = Http::withHeaders([
+                        'Authorization' => env('COURSEROOM_API_KEY'),
+                    ])->post($url.'/api/chats/buscar', [
+                        'IdUsuarioEmisor' => $idUsuarioEmisor,
+                        'Nombre' => $nombre,
+                        'Paterno' => $paterno,
+                        'Materno' => $materno
+                    ]);
+
+                    if ($response->ok()){
+
+                        $result = json_decode($response->body());
+
+                        return response()->json(['code' => 200 , 'data' => $result], 200);
+
+                    } else{
+                        return response()->json(['code' => 500 , 'data' => $response->body()], 200);
+                    }
+
+                } else{
+                    return response()->json(['code' => 404 , 'data' => 'Empty url'], 200);
+                }
+            }
+
+        } catch (\Throwable $th) {
+            return response()->json(['code' => 500 , 'data' => $th->getMessage()], 200);
+        }
+
+    }
+
+    public function chats_obtener(Request $request)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'IdUsuario' => ['required']
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['code' => 404 , 'data' => $validator->errors()->first()], 200);
+            } else {
+
+                $url = env('COURSEROOM_API');
+
+                $idUsuario = $request->input('IdUsuarioEmisor');
+
+                if($url != ''){
+
+                    $response = Http::withHeaders([
+                        'Authorization' => env('COURSEROOM_API_KEY'),
+                    ])->post($url.'/api/chats/obtener', [
+                        'IdUsuario' => $idUsuario
                     ]);
 
                     if ($response->ok()){
