c340e8497a018123f67ff2f0fbe507576f55ae5c
Brian GL <brian.gaytan4368@alumnos.udg.mx>
2023-01-08 20:29:21 -0600

    Corrección AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
