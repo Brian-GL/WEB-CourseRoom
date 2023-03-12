41672f29769d29721bcea2bc29fc76321c980648
Brian GL <brian.gaytan4368@alumnos.udg.mx>
2023-03-12 12:31:09 -0600

    Correccion AppserviceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,10 +23,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
         if (env('APP_ENV') != 'local') {
-            $url->forceScheme('https');
+            URL::forceSchema('https');
         }
     }
 }
